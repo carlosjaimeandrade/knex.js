93dd44fdcd7a696ebd89192b5fa9979aea750ca4
Carlos Jaime <jaime_andrek@hotmail.com>
2022-05-31 20:48:52 -0300

    insert com query Build

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const database = require('./database')
+
+const dados = [{
+    nome : "far cry",
+    preco : 127.5
+},
+{
+    nome : "far cry 2",
+    preco : 122
+}
+]
+
+database.insert(dados).into("games").then(data=>{
+    console.log(data)
+}).catch(err=>{
+    console.log(err)
+})
+
